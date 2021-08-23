0b4f6bbc5c99cd1d96b3cdd52a321d028db82294
justced <jced.delacarcel@gmail.com>
2021-08-23 21:46:28 +0800

    added create database

--- app/core/Application.php
+++ app/core/Application.php
@@ -2,5 +2,12 @@
     $PDO = new PDO('mysql:host=localhost;port=3306;dbname=wrs_db','root','');
     $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    try{
+        $SQL = "CREATE DATABASE wrs_db"
+        $PDO->exec($SQL);
+        echo "Database created successfully.";
+    } catch(PDOException $error){
+        die("Error: Could not able to execute $SQL." . $error->getMessage());
+    }
 
 ?>
