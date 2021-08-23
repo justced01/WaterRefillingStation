616477d6d33cb3a125b56d1aa9dfe648a1cca668
justced <jced.delacarcel@gmail.com>
2021-08-23 22:01:19 +0800

    added error message in connection

--- app/core/Application.php
+++ app/core/Application.php
@@ -1,9 +1,13 @@
 <?php
-    $PDO = new PDO('mysql:host=localhost;port=3306;dbname=wrs_db','root','');
-    $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    try {
+        $PDO = new PDO('mysql:host=localhost;port=3306;dbname=wrs_db','root','');
+        $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $error){
+        die("Error: Could not able to connect $SQL." . $error->getMessage());
+    }
 
     try{
-        $SQL = "CREATE DATABASE wrs_db"
+        $SQL = "CREATE DATABASE wrs_db";
         $PDO->exec($SQL);
         echo "Database created successfully.";
     } catch(PDOException $error){
